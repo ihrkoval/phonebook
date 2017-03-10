7ccdc872dfa93f6e0315e5423a041fd1e0d19f27
ihrkoval <karasique228>
2017-03-10 17:30:27 +0200

    dao change

--- src/main/java/com/phonebook/dao/UserDaoImpl.java
+++ src/main/java/com/phonebook/dao/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package com.phonebook.dao;
 
 import com.phonebook.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,29 +21,19 @@ public class UserDaoImpl implements UserDao {
     @Value("${spring.datasource.url}")
     String datasource;
 
-    public List<User> getAll() {
-        List<User> users = new ArrayList<>();
-        User u = new User();
-        System.out.println(datasource + " DATASOURCE");
-        try {
-            Connection conn = DriverManager.getConnection(datasource);
-            Statement stmt = conn.createStatement();
-            ResultSet results = stmt.executeQuery("SELECT * FROM Users");
-            int i = 0;
-            while (results.next())
-            {
-                i++;
-                System.out.println(results.getString("name") + " SIZEEEEEE");
-            }
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    @Autowired
+    private EntityManager entityManager;
 
-        /*u.setName("VASYA");
-        u.setId(1);*/
-        users.add(u);
-        return users;
+    private Statement statment() throws SQLException {
+        Connection conn = DriverManager.getConnection(datasource);
+        Statement stmt = conn.createStatement();
+        return stmt;
+    }
+
+
+    public List<User> getAll() {
+       List<User> usrs =  entityManager.createNativeQuery("SELECT * FROM Users", User.class).getResultList();
+        return usrs;
     }
 
 
